ee4dc375adef288c98cbbd24dd58ed3ff3e36d1e
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2022-06-29 12:36:47 +0900

    이중for문
    이중for문 실습

---
+++ Java0629/src/java0629/ex02.java
@@ -0,0 +1,21 @@
+package java0629;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		
+
+			
+		for(int j=1; j<=5; j++) {
+			for (int i =5; i >=j; i--) {
+
+				System.out.print("*");
+				}
+			System.out.println();
+
+
+			}
+		}
+	
+
+}
